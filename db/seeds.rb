f0c201e2ad860ec6c519b14d7ada04348e80fa79
Robert Pedersen <rob@worldcrimeleague.org>
2019-06-10 01:27:29 -0400

    followed along

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+article1 = Article.create(title: "The Test", description: "This is the description")
